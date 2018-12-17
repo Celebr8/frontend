f83efafa88f25d2d1ee9791c31a5853a6eb86c4e
Loic Coenen <ijwtcymb@gmail.com>
2018-12-17 17:04:36 +0000

    Issue 3, test A: entered the filter value from Go

--- src/marketplace-custom-config.js
+++ src/marketplace-custom-config.js
@@ -3,52 +3,110 @@
  */
 
 export const amenities = [
-  {
-    key: 'towels',
-    label: 'Towels',
-  },
-  {
-    key: 'bathroom',
-    label: 'Bathroom',
-  },
-  {
-    key: 'swimming_pool',
-    label: 'Swimming pool',
-  },
-  {
-    key: 'own_drinks',
-    label: 'Own drinks allowed',
-  },
-  {
-    key: 'jacuzzi',
-    label: 'Jacuzzi',
-  },
-  {
-    key: 'audiovisual_entertainment',
-    label: 'Audiovisual entertainment',
-  },
-  {
-    key: 'barbeque',
-    label: 'Barbeque',
-  },
-  {
-    key: 'own_food_allowed',
-    label: 'Own food allowed',
-  },
+	{
+		key: "Parking"
+	},
+	{
+		key: "Credit/Debit cards"
+	},
+	{
+		key: "Wheelchair access"
+	},
+	{
+		key: "Sport/Movie screenings"
+	},
+	{
+		key: "Outdoor area"
+	},
+	{
+		key: "Board games"
+	},
+	{
+		key: "Live music"
+	},
+	{
+		key: "Karaoke"
+	},
+	{
+		key: "Seating"
+	},
+	{
+		key: "Table service"
+	},
+	{
+		key: "Heating"
+	},
+	{
+		key: "Air Conditioner"
+	},
+	{
+		key: "Cocktails"
+	},
+	{
+		key: "Smoking area"
+	},
 ];
 
-export const categories = [
-  { key: 'smoke', label: 'Smoke' },
-  { key: 'electric', label: 'Electric' },
-  { key: 'wood', label: 'Wood' },
-  { key: 'other', label: 'Other' },
+export const regularyOpenOn = [
+	{
+		key: "Mondays"
+	},
+	{
+		key: "Tuesdays"
+	},
+	{
+		key: "Wednesdays"
+	},
+	{
+		key: "Thursdays"
+	},
+	{
+		key: "Fridays"
+	},
+	{
+		key: "Saturdays"
+	},
+	{
+		key: "Sundays"
+	},
+];
+
+export const groupSize = [
+	{
+		key: "1 - 10"
+	},
+	{
+		key: "11 - 20"
+	},
+	{
+		key: "21 - 30"
+	},
+	{
+		key: "31 - 40"
+	},
+	{
+		key: "41 - 50"
+	},
+	{
+		key: "51 - 60"
+	},
+	{
+		key: "61 - 70"
+	},
+	{
+		key: "71 - 80"
+	},
+	{
+		key: "81 - 90"
+	},
+	{
+		key: "91 - 100"
+	},
+	{
+		key: "More than 100"
+	},
 ];
 
 // Price filter configuration
 // Note: unlike most prices this is not handled in subunits
-export const priceFilterConfig = {
-  min: 0,
-  max: 1000,
-  step: 5,
-};
 
