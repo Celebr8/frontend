2fb7c4f7c0beea2e8856bcbc308be322f23930f0
Loic Coenen <ijwtcymb@gmail.com>
2018-12-23 15:09:57 +0000

    Issue 3b, test A: added two tabs to Filters
    We followed https://github.com/sharetribe/flex-template-web/blob/master/docs/extend-listing.md to the Show the attribute on listing page section.
    
    The code is still broken: the following error appears:
    
        ./src/forms/EditListingCapacityForm/EditListingCapacityForm.js
          Line 54:  Unexpected use of 'name'  no-restricted-globals
          Line 55:  Unexpected use of 'name'  no-restricted-globals
          Line 56:  'config' is not defined   no-undef

---
+++ src/forms/EditListingCapacityForm/EditListingCapacityForm.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { bool, func, shape, string } from 'prop-types';
+import { compose } from 'redux';
+import { Form as FinalForm } from 'react-final-form';
+import { intlShape, injectIntl, FormattedMessage } from 'react-intl';
+import classNames from 'classnames';
+import { propTypes } from '../../util/types';
+import { Form, Button, FieldCheckboxGroup } from '../../components';
+
+import css from './EditListingCapacityForm.css';
+
+export const EditListingCapacityFormComponent = props => (
+  <FinalForm
+    {...props}
+    render={fieldRenderProps => {
+      const {
+        className,
+        disabled,
+        handleSubmit,
+        intl,
+        invalid,
+        pristine,
+        saveActionMsg,
+        updated,
+        updateInProgress,
+        fetchErrors,
+      } = fieldRenderProps;
+
+
+      const { updateListingError, showListingsError } = fetchErrors || {};
+      const errorMessage = updateListingError ? (
+        <p className={css.error}>
+          <FormattedMessage id="EditListingCapacityForm.updateFailed" />
+        </p>
+      ) : null;
+      const errorMessageShowListing = showListingsError ? (
+        <p className={css.error}>
+          <FormattedMessage id="EditListingCapacityForm.showListingFailed" />
+        </p>
+      ) : null;
+
+      const classes = classNames(css.root, className);
+      const submitReady = updated && pristine;
+      const submitInProgress = updateInProgress;
+      const submitDisabled = invalid || disabled || submitInProgress;
+
+      return (
+        <Form className={classes} onSubmit={handleSubmit}>
+          {errorMessage}
+          {errorMessageShowListing}
+
+          <FieldCheckboxGroup
+            className={css.capacity}
+            id={name}
+            name={name}
+            options={config.custom.groupSize}
+          />
+
+          <Button
+            className={css.submitButton}
+            type="submit"
+            inProgress={submitInProgress}
+            disabled={submitDisabled}
+            ready={submitReady}
+          >
+            {saveActionMsg}
+          </Button>
+        </Form>
+      );
+    }}
+  />
+);
+
+EditListingCapacityFormComponent.defaultProps = {
+  selectedPlace: null,
+  updateError: null,
+};
+
+EditListingCapacityFormComponent.propTypes = {
+  intl: intlShape.isRequired,
+  onSubmit: func.isRequired,
+  saveActionMsg: string.isRequired,
+  selectedPlace: propTypes.place,
+  updated: bool.isRequired,
+  updateInProgress: bool.isRequired,
+  fetchErrors: shape({
+    showListingsError: propTypes.error,
+    updateListingError: propTypes.error,
+  }),
+};
+
+export default compose(injectIntl)(EditListingCapacityFormComponent);
