9f5055d00a542f23925cd5c635274150673f3ad1
Loic Coenen <ijwtcymb@gmail.com>
2018-12-30 12:44:30 +0000

    Issue 3c, test A: changed the category field to listing type
    Result: the section doesn't appears because of the Maybe

---
+++ src/forms/EditListingDescriptionForm/CustomListingTypeSelectFieldMaybe.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { required } from '../../util/validators';
+import { FieldSelect } from '../../components';
+
+import css from './EditListingDescriptionForm.css';
+
+const CustomListingTypeSelectFieldMaybe = props => {
+  const { name, id, listingTypes, intl } = props;
+  const listingTypeLabel = intl.formatMessage({
+    id: 'EditListingDescriptionForm.listingTypeLabel',
+  });
+  const listingTypePlaceholder = intl.formatMessage({
+    id: 'EditListingDescriptionForm.listingTypePlaceholder',
+  });
+  const listingTypeRequired = required(
+    intl.formatMessage({
+      id: 'EditListingDescriptionForm.listingTypeRequired',
+    })
+  );
+  return listingTypes ? (
+    <FieldSelect
+      className={css.listingType}
+      name={name}
+      id={id}
+      label={listingTypeLabel}
+      validate={listingTypeRequired}
+    >
+      <option disabled value="">
+        {listingTypePlaceholder}
+      </option>
+      {listingTypes.map(c => (
+        <option key={c.key} value={c.key}>
+          {c.label}
+        </option>
+      ))}
+    </FieldSelect>
+  ) : null;
+};
+
+export default CustomListingTypeSelectFieldMaybe;
