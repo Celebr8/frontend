8b761216c99d973ec391bc4adc1cae36a00ba531
BuckWierd <radu@whichost.com>
2019-02-28 19:26:20 +0000

    Testing the deals page
    For Loic to check why I'm getting a blank page.

--- src/components/BirthdayDeal/BirthdayDeal.js
+++ src/components/BirthdayDeal/BirthdayDeal.js
@@ -16,6 +16,19 @@ const BirthdayDeal  = props => {
 	  <p>We support your celebration because we care.</p>
 	  <p><span>Follow these three simple steps and we will reward you with up to <b>€ 200</b></span></p>
 	  
+	  <div className={css.post_container}>
+  <div className={css.image}
+        style="background-image:url('https://ae01.alicdn.com/kf/HTB13zAXIFXXXXbxXFXXq6xXFXXX7/New-Arrival-2014-Mens-Flared-Jeans-Men-s-Bell-Bottom-Denim-Male-Big-Horn-Jean-Flare.jpg');">
+  </div>
+  <div className={css.desc}>
+  <h3 className={css.postTitle}>Post title</h3>
+   <p>Description here asjhd skdhf kasdhf askdfh ;alskdf lasdfj askdjfh kasdhf kasjd hf ere asjhd skdhf kasdhf askdfh ;alskdf lasdfj askdjfh kasdhf kasjd hf ere asjhd skdhf kasdhf askdfh ;alskdf lasdfj askdjfh kasdhf kasjd hfere asjhd skdhf kasdhf askdfh ;alskdf lasdfj askdjfh kasdhf kasjd hf
+   </p>
+  </div>
+  <div className={css.readMore}>
+  <a href="#" target="_blank">Read more about the terms &nbsp;<i style="font-size:18px" className={css.fa}>&#xf101;</i></a>
+  </div>
+</div>
  
     </div>
     
