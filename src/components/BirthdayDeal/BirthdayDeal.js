e03646fc203b646d3d366f6c410c2a3ff768b34d
BuckWierd <radu@whichost.com>
2019-03-05 18:39:44 +0000

    Birthday deal basic set up
    Setted up the layout of the birthday deal. Will be continued.

--- src/components/BirthdayDeal/BirthdayDeal.js
+++ src/components/BirthdayDeal/BirthdayDeal.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import css from './BirthdayDeal.css';
+import image1 from './img/birthday_deal_image_01.png';
+import image2 from './img/birthday_deal_image_02.png';
+import image3 from './img/birthday_deal_image_03.png';
 
 const BirthdayDeal  = props => {
   const { rootClassName, className } = props;
@@ -13,23 +16,54 @@ const BirthdayDeal  = props => {
 	  
     <div className={classes}>
     
-	  <p>We support your celebration because we care.</p>
-	  <p><span>Follow these three simple steps and we will reward you with up to <b>€ 200</b></span></p>
-	  
-	  <div className={css.post_container}>
-  <div className={css.image}
-        style="background-image:url('https://ae01.alicdn.com/kf/HTB13zAXIFXXXXbxXFXXq6xXFXXX7/New-Arrival-2014-Mens-Flared-Jeans-Men-s-Bell-Bottom-Denim-Male-Big-Horn-Jean-Flare.jpg');">
-  </div>
-  <div className={css.desc}>
-  <h3 className={css.postTitle}>Post title</h3>
-   <p>Description here asjhd skdhf kasdhf askdfh ;alskdf lasdfj askdjfh kasdhf kasjd hf ere asjhd skdhf kasdhf askdfh ;alskdf lasdfj askdjfh kasdhf kasjd hf ere asjhd skdhf kasdhf askdfh ;alskdf lasdfj askdjfh kasdhf kasjd hfere asjhd skdhf kasdhf askdfh ;alskdf lasdfj askdjfh kasdhf kasjd hf
-   </p>
-  </div>
-  <div className={css.readMore}>
-  <a href="#" target="_blank">Read more about the terms &nbsp;<i style="font-size:18px" className={css.fa}>&#xf101;</i></a>
-  </div>
-</div>
- 
+		<section className={css.container}>
+	  			<p>We support your celebration because we care.</p>
+	  			<p><span>Follow these three simple steps and we will reward you with up to <b>€ 200</b></span></p>
+	  	</section>
+	  		
+	  	<section className={css.boxes}>
+	  		<div className={css.container}>
+	  			<div className={css.boxLeft}>
+	  				<img src={image1}></img>
+	  				<h3>Happy Birthday!</h3>
+	  				<p>At Whichost it's all about your day! That's why we reward you for celebrating.</p>
+	  				<p>Search for a pub, select the type "Birthday" and book it.</p>
+	  				<section className={css.readMore}>
+	  				<a href="#" target="_blank">Browse pubs now -></a>
+	  				</section>
+	  			</div>
+	  		</div>
+	  	</section>
+	  	
+	  	<section className={css.boxes}>
+	  		<div className={css.container}>
+	  			<div className={css.boxRight}>
+	  				<img src={image2}></img>
+	  				<h3>Have a great time!</h3>
+	  				<p>Take your friends and show them how to party but don't forget to not leave a mess behind. It's all about leaving the place like you would like to find it next time.</p>
+	  				<section className={css.readMore}>
+	  				<a href="#" target="_blank">Community guidelines -></a>
+	  				</section>
+	  			</div>
+	  		</div>
+	  	</section>
+	  	
+	  	<section className={css.boxes}>
+	  		<div className={css.container}>
+	  			<div className={css.boxLeft}>
+	  				<img src={image3}></img>
+	  				<h3>Let us know how it was!</h3>
+	  				<p>We are excited about this special day!</p>
+	  				<p>Gather your party group together, take a picture, post it on social media  and let us know how your party was.</p>
+	  				<section className={css.readMore}>
+	  				<a href="#" target="_blank">Learn about the deal terms -></a>
+	  				</section>
+	  			</div>
+	  		</div>
+	  	</section>
+	  		
+
+   
     </div>
     
     
