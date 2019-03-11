0a631565d8bf75f7759f888b0204c015f42ceb29
Loic Coenen <ijwtcymb@gmail.com>
2019-03-11 20:47:56 +0000

    Issue #85, accordion created
    No styling have been done yet

---
+++ src/components/BirthdayDeal/examples.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export const examplesContent = (
+	<div>
+		<p>Below you will find examples of each of the benefits and how to avail them.</p>
+		<ol>
+			<li><b>Example explaining how to avail the 100% refund benefit (pub XYZ is only used as a reference):</b>
+				<ol>
+					<li>Aoife registers to Whichost.</li>
+					<li>Aoife completes her profile and indicates her birthday in it.</li>
+					<li>Aoife requests a booking at pub XYZ that is managed by Whichost, to celebrate her birthday.</li>
+					<li>Pub XYZ confirms the booking.</li>
+					<li>Aoife’s 100% refund is triggered as soon as pub XYZ confirms the booking.</li>
+				</ol>
+			</li>
+			<li><b>Example explaining how to avail the €5.00 - €10.00, plus €0.50 or €1.00 per additional person for parties of more than 10 people, Amazon Gift Card benefit by posting on Instagram/Facebook/Twitter:</b>
+				<ol>
+					<li>Aoife registers to Whichost.</li>
+					<li>Aoife completes her profile and indicates her birthday in it.</li>
+					<li>Aoife requests a booking for a party of 20 people at a pub through Whichost, to celebrate her birthday</li>
+					<li>The pub confirms the booking.</li>
+					<li>Aoife’s 100% refund is triggered as soon as the pub confirms the booking (if the listing is managed by Whichost).</li>
+					<li>Aoife enjoys the party and while at it she posts an image of the party on her Instagram/Facebook/Twitter account, tagging Whichost in that post.</li>
+					<li>Aoife keeps the post live for at least a full week (7 days) on her Instagram/Facebook/Twitter account.</li>
+					<li>Aoife writes a review for the pub on Whichost.</li>
+					<li>Whichost checks the Instagram/Facebook/Twitter post and the bookings in the platform and if everything matches, they send:
+						<ol>
+							<li>A €10.00 (€5.00 + €0.50 per additional person (10)) Amazon Gift Card to Aoife’s email address (the one registered in her Whichost account) if the party was held on a Thursday, Friday, Saturday or Sunday.</li>
+							<li>A €20.00 (€10.00 + €1.00 per additional person (10)) Amazon Gift Card to Aoife’s email address (the one registered in her Whichost account) if the party was held on a Monday, Tuesday or Wednesday.</li>
+						</ol>
+					</li>
+					<li>If the information does not match, for any reason, Whichost will contact Aoife to clarify.</li>
+				</ol>
+			</li>
+			<li><b>Example explaining how to avail the €10.00 - €15.00, plus €0.50 or €1.00 per additional person for parties of more than 10 people, Amazon Gift Card benefit by posting on Instagram/Facebook/Twitter (pub ABC is only used as a reference):</b>
+				<ol>
+					<li>Aoife registers to Whichost.</li>
+					<li>Aoife completes her profile and indicates her birthday in it.</li>
+					<li>Aoife recommends a pub ABC to Whichost through the ‘Recommend a pub’ form inside Whichost.</li>
+					<li>Whichost adds the pub ABC to the platform and replies to Aoife confirming she can now book the party.</li>
+					<li>Aoife requests a booking for a party of 20 people at pub ABC through Whichost, to celebrate her birthday.</li>
+					<li>The pub confirms the booking.</li>
+					<li>Aoife’s 100% refund is triggered as soon as the pub confirms the booking (if the listing is managed by Whichost).</li>
+					<li>Aoife enjoys the party and while at it she posts an image of the party on her Instagram/Facebook/Twitter account, tagging Whichost in that post.</li>
+					<li>Aoife keeps the post live for at least a full week (7 days) on her Instagram/Facebook/Twitter account.</li>
+					<li>Aoife writes a review for the pub on Whichost.</li>
+					<li>Whichost checks the Instagram/Facebook/Twitter post and the bookings in the platform and if everything matches, they send:
+						<ol>
+							<li>A €15.00 (€10.00 + €0.50 per additional person (10)) Amazon Gift Card to Aoife’s email address (the one registered in her Whichost account) if the party was held on a Thursday, Friday, Saturday or Sunday.</li>
+							<li>A €25.00 (€15.00 + €1.00 per additional person (10)) Amazon Gift Card to Aoife’s email address (the one registered in her Whichost account) if the party was held on a Monday, Tuesday or Wednesday.</li>
+						</ol>
+					</li>
+					<li>If the information does not match, for any reason, Whichost will contact Aoife to clarify.</li>
+				</ol>
+			</li>
+		</ol>
+	</div>
+);
+
