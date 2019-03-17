6a92584d35e2bb28a0147fcc6933c400291e3860
Loic Coenen <ijwtcymb@gmail.com>
2019-03-17 19:29:44 +0000

    Issue #85, fixed the "left menu" text issue

--- src/components/Deals/Deals.js
+++ src/components/Deals/Deals.js
@@ -13,7 +13,7 @@ const Deals = props => {
         <section>
 		<h1>Part of our culture is giving back</h1>
         <p> On this page you will find all the ways in which we'll give back to our community and customers.</p>
-        <p>Take a look at the menu on the left side and you'll see our active deals for you as a loyal customer. We call them <b>"The Whichost Deals"</b>.</p>
+        <p>Take a look at the menu and you'll see our active deals for you as a loyal customer. We call them <b>"The Whichost Deals"</b>.</p>
         <p>Click for example on <b>"Birthday deal"</b>, read through the details and don't hesitate to <u><a href="mailto:support@whichost.com">write us a email</a></u>, in case you have further questions. You can also <u><a href="https://info.whichost.com/eng/book-a-call">book a call</a></u> and we'll get back to you over the phone.</p>
         <br></br>
         <h3>Have fun!</h3>
