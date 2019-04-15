77bada3270aff65cc6bec94e7f1ac08d6b83b669
Loic Coenen <ijwtcymb@gmail.com>
2019-04-15 16:39:54 +0100

    Change URLS of legals page #140

--- src/routeConfiguration.js
+++ src/routeConfiguration.js
@@ -292,27 +292,27 @@ const routeConfiguration = () => {
       component: props => <CorporateDealPage {...props} />,
     },
     {
-			path: '/legals/terms-of-service',
+			path: '/legal/terms-of-service',
       name: 'TermsOfServicePage',
       component: props => <TermsOfServicePage {...props} />,
     },
     {
-			path: '/legals/privacy-policy',
+			path: '/legal/privacy-policy',
       name: 'PrivacyPolicyPage',
       component: props => <PrivacyPolicyPage {...props} />,
     },
     {
-			path: '/legals/non-discrimination',
+			path: '/legal/non-discrimination',
       name: 'NonDiscriminationPolicyPage',
       component: props => <NonDiscriminationPolicyPage {...props} />,
     },
     {
-			path: '/legals/cookie-policy',
+			path: '/legal/cookie-policy',
       name: 'CookiePolicyPage',
       component: props => <CookiePolicyPage {...props} />,
     },
     {
-			path: '/legals/community-guidelines',
+			path: '/legal/community-guidelines',
       name: 'CommunityGuidelinesPage',
       component: props => <CommunityGuidelinesPage {...props} />,
     },
