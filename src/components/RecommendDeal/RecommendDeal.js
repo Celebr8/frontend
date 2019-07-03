43090acbbe99ba784ace7f0ad275537d48bf77db
Scott Kennedy <scottyzen@gmail.com>
2019-07-03 21:35:14 +0100

    Added chart to Recomend rewards page

--- src/components/RecommendDeal/RecommendDeal.js
+++ src/components/RecommendDeal/RecommendDeal.js
@@ -2,8 +2,8 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { NamedLink } from '..';
+import { PricingTable } from './pricingTable';
 import css from './RecommendDeal.css';
-
 
 
 
@@ -20,19 +20,10 @@ const RecommendDeal = props => {
 
 		<div className={css.container}>
 			<div className={classes}>
-				<h2>Earn a gift card to use with over 2,000 shops in Ireland &amp; the UK!<br />
-				Get up to €100 for recommending a pub and celebrating there.</h2>
-				<section className={css.getInTouch}>
-					<div className={css.btnGroup + ' ' + css.containerButton}>
-						<section className={css.centerButtons}>
-							<a href="https://docs.google.com/forms/d/e/1FAIpQLSfr2dEghUT1VF1i9BWPsvZIJpnHe2FPVmCuoHJaAR3AXDe_GA/viewform" target="_blank"><button type="submit">Recommend a pub</button></a>
-						</section>
-					</div>
-				</section>
+				<h1>Get up to €100 for recommending a pub and celebrating there.</h1>
 				<div className={css.steps}>
 					<div>
-						<p>
-						We are always adding pubs to our platform, so when you want to celebrate in a pub that we don't have, not to worry since you can still celebrate there.
+						<p>Earn a gift card to use with over 2,000 shops in Ireland &amp; the UK! We are always adding pubs to our platform, so when you want to celebrate in a pub that we don't have, not to worry since you can still celebrate there.
 						</p>
 						<p>
 						When you recommend a pub, we will collect information on it so that the listing can be created and you can book your party there. We will issue the gift card after you’ve held your party with them. The value of it depends on the size of your party.<br />
@@ -40,10 +31,18 @@ const RecommendDeal = props => {
 						</p>
 					</div>
 				</div>
+				<PricingTable />
 			</div>
-			<div className={css.readMore}>
-				<NamedLink name="LegalsDealsPage">Recommend Reward Terms &amp; Conditions</NamedLink>
-			</div>
+			<section className={css.getInTouch}>
+				<div className={css.btnGroup + ' ' + css.containerButton}>
+					<section className={css.centerButtons}>
+						<a href="https://docs.google.com/forms/d/e/1FAIpQLSfr2dEghUT1VF1i9BWPsvZIJpnHe2FPVmCuoHJaAR3AXDe_GA/viewform" target="_blank"><button type="submit">Recommend a pub</button></a>
+					</section>
+					<div className={css.readMore}>
+						<NamedLink name="LegalsDealsPage">Recommend Reward Terms &amp; Conditions</NamedLink>
+					</div>
+				</div>
+			</section>
 		</div>
 
 	);
--- src/components/RecommendDeal/pricingTable.js
+++ src/components/RecommendDeal/pricingTable.js
@@ -1,19 +1,18 @@
-import React from 'react';
-
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import React from 'react';
+
 
 
 export const PricingTable = (props) => (
 	<Table>
 		<TableHead>
 			<TableRow>
-				<TableCell>Party size<br/>(People)</TableCell>
-				<TableCell align="center">Gift card value<br/>MO - WE</TableCell>
-				<TableCell align="center">Gift card value<br/>TH - SU</TableCell>
+				<TableCell>Party Size<br/>(People)</TableCell>
+				<TableCell align="center">Gift Card<br/>Value</TableCell>
 			</TableRow>
 		</TableHead>
 		<TableBody>
@@ -21,36 +20,31 @@ export const PricingTable = (props) => (
 				<TableCell component="th" scope="row">
 					Up to 30
 				</TableCell>
-				<TableCell align="center">€ 25.00</TableCell>
 				<TableCell align="center">€ 15.00</TableCell>
 			</TableRow>
 			<TableRow key="UNIQUE_ID2">
 				<TableCell component="th" scope="row">
 					31 - 50
 				</TableCell>
-				<TableCell align="center">€ 45.00</TableCell>
-				<TableCell align="center">€ 35.00</TableCell>
+				<TableCell align="center">€ 20.00</TableCell>
 			</TableRow>
 			<TableRow key="UNIQUE_ID3">
 				<TableCell component="th" scope="row">
 					51 - 100
 				</TableCell>
-				<TableCell align="center">€ 75.00</TableCell>
-				<TableCell align="center">€ 60.00</TableCell>
+				<TableCell align="center">€ 35.00</TableCell>
 			</TableRow>
 			<TableRow key="UNIQUE_ID4">
 				<TableCell component="th" scope="row">
 					101 - 200
 				</TableCell>
-				<TableCell align="center">€ 130.00</TableCell>
-				<TableCell align="center">€ 115.00</TableCell>
+				<TableCell align="center">€ 60.00</TableCell>
 			</TableRow>
 			<TableRow key="UNIQUE_ID5">
 				<TableCell component="th" scope="row">
 					201+
 				</TableCell>
-				<TableCell align="center">€ 230.00</TableCell>
-				<TableCell align="center">€ 200.00</TableCell>
+				<TableCell align="center">€ 100.00</TableCell>
 			</TableRow>
 
 		</TableBody>
