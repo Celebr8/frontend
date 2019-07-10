ff8c19b591e3dad6dc9e0a6a091e923bb0c491fa
Shane Walsh <walsh.shane@icloud.com>
2019-07-10 13:36:05 +0100

    Implement and style OR divider component #275

---
+++ src/components/OrDivider/OrDivider.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import css from './OrDivider.css.css';
+
+class OrDivider extends Component {
+  render() {
+    return <div className={css.orDivider}>OR</div>;
+  }
+}
+export default OrDivider;
