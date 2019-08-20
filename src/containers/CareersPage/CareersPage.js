7fc94708bd0e60828404ae7516b9f43f633ef39a
Carlos R <39112974+RohrerHope@users.noreply.github.com>
2019-08-20 12:07:05 +0100

    CareersPage.js update Whichost to Celebr8
    No urls, codes, legal names or emails changed.

--- src/containers/CareersPage/CareersPage.js
+++ src/containers/CareersPage/CareersPage.js
@@ -45,7 +45,7 @@ const CareersPageComponent = props => {
 				</LayoutWrapperHero>
 				<LayoutWrapperMain>
 				<section>
-					<h1>Shape the future of Whichost</h1>
+					<h1>Shape the future of Celebr8</h1>
 						<b>Position:</b> Frontend Engineer (Co-Founder)
 						<br/>
 						<b>Department:</b> Founding team
@@ -55,15 +55,15 @@ const CareersPageComponent = props => {
 				<section>
 					<br/>
 					<h2>About the company</h2>
-					<p> At Whichost, we aim to leave this world better than we found it. To achieve this we work with talented people and go above and beyond for our users. <b>We are an early stage startup with initial revenue</b> and a self-funded business with no investors or outside influences except one: our users and what's best for them. We are a startup of execution, full of triers and doers: we try things, we make mistakes, and we learn from them.</p>
+					<p> At Celebr8, we aim to leave this world better than we found it. To achieve this we work with talented people and go above and beyond for our users. <b>We are an early stage startup with initial revenue</b> and a self-funded business with no investors or outside influences except one: our users and what's best for them. We are a startup of execution, full of triers and doers: we try things, we make mistakes, and we learn from them.</p>
 					<p>Starting this company in Cork is an amazing journey to be a part of. As an early stage startup we don't have fancy offices. However, we do work in fancy coffee shops.</p>
-					<p>We are seeking a Frontend Engineer Co-founder for our company to accelerate the improvement of our platform, so you won’t have to start from zero! You will be assigned significant equity participation in the company. You will not be required to leave your day job (if you have one) for the time being until we have enough revenue or resources to pay ourselves a living wage. You will also not be required to relocate to Cork in case you are located in a different location. However, after 6 PM we do live and breathe Whichost, including weekends and holidays (keeping a healthy balance with our personal lives).</p>
+					<p>We are seeking a Frontend Engineer Co-founder for our company to accelerate the improvement of our platform, so you won’t have to start from zero! You will be assigned significant equity participation in the company. You will not be required to leave your day job (if you have one) for the time being until we have enough revenue or resources to pay ourselves a living wage. You will also not be required to relocate to Cork in case you are located in a different location. However, after 6 PM we do live and breathe Celebr8, including weekends and holidays (keeping a healthy balance with our personal lives).</p>
 					<p>Your responsibilities will be define together with the team, once you are selected!</p>
 					<br/>
-					<ol><p>Meet the Whichost team:</p>
-						<li><b>Loic C.:</b> Frontend Engineer with a major in Psychology. Joined Whichost in November 2018. <a href="https://angel.co/loic-coenen" target="_blank">AngelList profile</a></li>
-						<li><b>Radu S.:</b> Electrical Engineer with 3 startups under his belt. Joined Whichost in July 2018. <a href="https://angel.co/radu-sighencea" target="_blank">AngelList profile</a></li>
-						<li><b>Carlos R.:</b> Industrial Engineer with 4 startups under his belt. Founded Whichost in November 2017. <a href="https://angel.co/carloswhichost" target="_blank">AngelList profile</a></li>
+					<ol><p>Meet the Celebr8 team:</p>
+						<li><b>Loic C.:</b> Frontend Engineer with a major in Psychology. Joined Celebr8 in November 2018. <a href="https://angel.co/loic-coenen" target="_blank">AngelList profile</a></li>
+						<li><b>Radu S.:</b> Electrical Engineer with 3 startups under his belt. Joined Celebr8 in July 2018. <a href="https://angel.co/radu-sighencea" target="_blank">AngelList profile</a></li>
+						<li><b>Carlos R.:</b> Industrial Engineer with 4 startups under his belt. Founded Celebr8 in November 2017. <a href="https://angel.co/carloswhichost" target="_blank">AngelList profile</a></li>
 					</ol>
 				</section>
 				<section>
@@ -98,8 +98,8 @@ const CareersPageComponent = props => {
 				<section>
 					<br/>
 					<h2>What you can expect</h2>
-					<p>Being part of Whichost isn't like a normal job. We are a startup! We believe in bringing onboard only the best, and as co-founders, we will make all the necessary sacrifices to make Whichost as big as possible.</p>
-					<p>Whichost was born out of a dream. A dream to make finding spaces to host your parties a breeze for everyone, and when we achieve it, we will not stop there. Whichost is a stepping-stone towards bigger dreams, more solutions for everyday issues, and better quality of life for every user around the world that we can reach.</p>
+					<p>Being part of Celebr8 isn't like a normal job. We are a startup! We believe in bringing onboard only the best, and as co-founders, we will make all the necessary sacrifices to make Celebr8 as big as possible.</p>
+					<p>Celebr8 was born out of a dream. A dream to make finding spaces to host your parties a breeze for everyone, and when we achieve it, we will not stop there. Celebr8 is a stepping-stone towards bigger dreams, more solutions for everyday issues, and better quality of life for every user around the world that we can reach.</p>
 					<p>As co-founders and entrepreneurs, we will continuously work to leave this world better than we found it!</p>
 					<section className={css.getInTouch}>
 					<h3>Sounds epic?</h3>
